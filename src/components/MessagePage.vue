6fff0a517471a369c1744008702d2b5c44efb1e8
gyamficodes <gyamfijohn08@gmail.com>
2023-04-30 10:57:36 +0000

    lesson

---
+++ src/components/MessagePage.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <!-- avatar -->
+    <!-- message -->
+   <!-- date -->
+</template>
+
+<script>
+
+    export default {
+        name:'MessagePage',
+        components:{},
+        props:[],
+        data(){
+            return{}
+        },
+        computed:{},
+        mounted(){},
+        watch:{}
+    }
+</script>
+
+<style lang="scss">
+</style>
+
